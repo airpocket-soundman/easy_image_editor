c31e29fd7a01b8e90d52deb594fb6bbe4045dda5
airpocket-soundman <yamashita.3154@gmail.com>
2026-07-15 17:02:01 +0900

    列数切替(3/4/5列)とマスサイズの自動計算 (v14)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* フォトベンチ Service Worker
    キャッシュ更新時は CACHE_VERSION を上げること */
 'use strict';
-const CACHE_VERSION = 'photobench-v13';
+const CACHE_VERSION = 'photobench-v14';
 const ASSETS = [
   './',
   './index.html',
