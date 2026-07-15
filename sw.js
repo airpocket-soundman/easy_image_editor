5688dbf39254b6fcd7cc5fa2bbed62d042712368
airpocket-soundman <yamashita.3154@gmail.com>
2026-07-15 17:21:21 +0900

    内接/外接UIの状態同期・余白を黒に・編集エリア背景をグレーに (v15)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* フォトベンチ Service Worker
    キャッシュ更新時は CACHE_VERSION を上げること */
 'use strict';
-const CACHE_VERSION = 'photobench-v14';
+const CACHE_VERSION = 'photobench-v15';
 const ASSETS = [
   './',
   './index.html',
