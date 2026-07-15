9a0456a37d4b94dd023a6e4a991e7108942a94d4
airpocket-soundman <yamashita.3154@gmail.com>
2026-07-15 18:35:44 +0900

    保存を完全バックグラウンド化・一覧反映の保険追加 (v22)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* フォトベンチ Service Worker
    キャッシュ更新時は CACHE_VERSION を上げること */
 'use strict';
-const CACHE_VERSION = 'photobench-v21';
+const CACHE_VERSION = 'photobench-v22';
 const ASSETS = [
   './',
   './index.html',
