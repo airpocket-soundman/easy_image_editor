f90903bbfa26f5dc41ba2b111506744b5ddb6a0d
airpocket-soundman <yamashita.3154@gmail.com>
2026-07-15 14:59:01 +0900

    回転後に塗りつぶし・ペン・矢印が消えるバグを修正

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* フォトベンチ Service Worker
    キャッシュ更新時は CACHE_VERSION を上げること */
 'use strict';
-const CACHE_VERSION = 'photobench-v2';
+const CACHE_VERSION = 'photobench-v4';
 const ASSETS = [
   './',
   './index.html',
